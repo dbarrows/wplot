e2a8c6a1967caac175a22e0fd262c57c289c29cf
Dexter Barrows <dexter@barrows.io>
2019-11-26 14:40:24 -0500

    add initial theme function file

---
+++ R/theme_space.R
@@ -0,0 +1,45 @@
+library(ggplot2)
+suppressMessages(library(showtext))
+font_add("space-mono", regular = "SpaceMono-Regular.ttf")
+font_add("open-sans", regular = "OpenSans-Regular.ttf")
+showtext_auto()
+
+black <- "#000000"
+gray <- "#808080"
+white <- "#ffffff"
+
+bgcolor <- white
+textcolor <- black
+gridcolor <- black
+tickcolor <- black
+
+default_color <- black
+fill_color <- gray
+update_geom_defaults("line", list(color = default_color, size = 0.35))
+update_geom_defaults("abline", list(color = default_color))
+update_geom_defaults("point", list(color = default_color))
+update_geom_defaults("area", list(fill = fill_color, color = bgcolor, size = 1))
+update_geom_defaults("bar", list(fill = gray))
+update_geom_defaults("col", list(fill = gray))
+
+theme_space <- function(base_size = 8, grid = TRUE, mono = FALSE) {
+    family <- ifelse(mono, "space-mono", "open-sans")
+    theme_bw(base_size = base_size) +
+    theme(
+        line = element_line(),
+        rect = element_rect(fill = bgcolor, linetype = 0, colour = NA),
+        text = element_text(size = base_size, color = textcolor, family = family),
+        axis.title = element_text(),
+        axis.title.x = element_text(margin = margin(15, 0, 0, 0)),
+        axis.title.y = element_text(margin = margin(0, 15, 0, 0)),
+        axis.text = element_text(size = base_size - 1, family = family, color = tickcolor),
+        axis.ticks = element_blank(),
+        legend.key = element_rect(fill = bgcolor),
+        panel.grid = element_blank(),
+        panel.grid.major = element_line(colour = gridcolor, linetype = "dotted", size = ifelse(grid, 0.1, 0)),
+        plot.title = element_text(hjust = -0.2, vjust = 2, size = rel(1.2), color = textcolor),
+        plot.margin = margin(0.75, 0.75, 0.75, 0.75, "cm"),
+        plot.background = element_rect(),
+        strip.background = element_rect(fill = "transparent")
+    )
+}
