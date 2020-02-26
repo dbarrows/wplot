b92f479719fa67b7bcb90752becaa6b0fc6fe272
Dexter Barrows <dexter@barrows.io>
2020-02-26 11:34:47 -0500

    expose font loading function

--- R/fonts.R
+++ R/fonts.R
@@ -1,6 +1,10 @@
+#' Load emplot fonts
+#'
+#' @export
 load_fonts <- function() {
     font_paths(system.file("fonts", package = "emplot"))
     font_add("et-book", regular = "et-book-roman-line-figures.ttf")
     font_add("lato", regular = "Lato-Regular.ttf")
     showtext_auto()
+    invisible()
 }
