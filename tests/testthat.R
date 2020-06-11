811ab223bf420be231375ac598f8ea849ae40bb7
Dexter Barrows <dexter@barrows.io>
2020-06-10 20:38:09 -0400

    add loading tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(wplot)
+
+test_check("wplot")
---
+++ tests/testthat/test-themes.R
@@ -0,0 +1,16 @@
+test_that("Themes load", {
+    p <- ggplot2::ggplot(ggplot2::mpg, aes(displ)) +
+            ggplot2::geom_histogram()
+    expect_silent({
+        p + theme_wl()
+    })
+    expect_silent({
+        p + theme_wd()
+    })
+    expect_silent({
+        p + theme_wc()
+    })
+    expect_silent({
+        p + theme_wt()
+    })
+})
